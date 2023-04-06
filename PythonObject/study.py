d30094eb5bc963e249b236088228512844c5033c
Den <90329284+zadumka@users.noreply.github.com>
2023-04-06 10:31:20 +0300

    text file

--- PythonObject/study.py
+++ PythonObject/study.py
@@ -2,3 +2,9 @@ d = [1, 2, 3, 4]
 print(d)
 a = 2 < 3
 print(a)
+
+myFile = open('file.txt')
+
+myList = myFile.read().split(' ')
+print(myList)
+
---
+++ task_ipr.py
@@ -0,0 +1,42 @@
+# Считать из текстового файла числа, написанные через пробел, записать их в переменную типа list.
+# Обработать этот список, выделив все простые числа в отдельный список.
+# Вывести получившийся список простых чисел.
+
+myFile = open('task.txt')
+
+myList = myFile.read().split(' ')
+print(myList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#numerList = []
+#for item in myList:
+ #   item = item.replace(',','').replace('.','')
+  #  if item.isdigit():
+   #     num = int(item)
+    #    if num > 1:
+     #       for i in range(2,num):
+      #          if num % i == 0:
+       #             break
+        #    else:
+         #       numerList.append(num)
+
+#print(numerList)
+
+
+
+
+
+
