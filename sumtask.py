aaf824980bd814714a83f0142711f0988e340a62
Den <90329284+zadumka@users.noreply.github.com>
2023-04-06 18:11:17 +0300

    Add new file

---
+++ sumtask.py
@@ -0,0 +1,21 @@
+myFile = open('file.txt')
+myList = myFile.read().split(' ')
+print(myList)
+
+numList = [str(item).replace(',','').replace('.', '') for item in myList]
+print(numList)
+
+newList = []
+for item in numList:
+    if item.isdigit():
+        num = int(item)
+        if num > 1:
+            print(range(2,num))
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+            else:
+                newList.append(int(item))
+print(newList)
+
+
