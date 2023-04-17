993bf7206dec557253f33e2b30329ebb819daa3e
Den <90329284+zadumka@users.noreply.github.com>
2023-04-17 11:52:52 +0300

    add file function and add sum task to the hile

---
+++ functions.py
@@ -0,0 +1,37 @@
+def function1():
+    print('Hello world')
+
+
+print('Hello from out')
+
+
+def function1():
+    print('Hello world')
+
+
+function1()
+
+
+def function2(x):
+    return 2 * x
+
+
+a = function2(10)
+print(a)
+
+
+def function3():
+    return 5
+
+
+print(function3())
+
+
+def function4(x):
+    print(x)
+    print('Good day')
+    return 3 * x
+
+
+a = function4(10)
+print(a)
