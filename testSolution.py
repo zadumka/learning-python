588deac75ad915d46a2788e0e27ef2fb11b9e360
Den <90329284+zadumka@users.noreply.github.com>
2023-05-19 18:24:21 +0300

    solutiontask

--- testSolution.py
+++ testSolution.py
@@ -2,38 +2,137 @@
 st = 'Print only spring the words that start with s in this sentence scrole'
 
 for word in st.split():
-    if word[0]== 's':
+    if word[0] == 's':
         print(word)
-#! Use range() to print all the even numbers from 0 to 10.
+# ! Use range() to print all the even numbers from 0 to 10.
 for i in range(0, 11, 2):
     print(i)
 
 # ! Use a List Comprehension to create a list of all numbers between 1 and 50 that are divisible by 3.
 
-mylist = [ num for num in range(1,50) if num %3 == 0]
+mylist = [num for num in range(1, 50) if num % 3 == 0]
 print(mylist)
 
 # ! Go through the string below and if the length of a word is even print "even!"
 st = 'Print every word in this sentence that has an even number of letters'
 for word in st.split():
-    if len(word)%2 == 0:
-        print(word+" <-- has an even length!")
+    if len(word) % 2 == 0:
+        print(word + " <-- has an even length!")
 
 # ! Write a program that prints the integers from 1 to 100.
 # ! But for multiples of three print "Fizz" instead of the number, and for the multiples of five print "Buzz".
 # !For numbers which are multiples of both three and five print "FizzBuzz".
 
-for i in range(1,100):
-    if i %3 == 0 and i %5==0:
+for i in range(1, 100):
+    if i % 3 == 0 and i % 5 == 0:
         print('FizzBuzz')
-    elif i %5 == 0:
+    elif i % 5 == 0:
         print('Buzz')
-    elif i %3==0:
+    elif i % 3 == 0:
         print('Fizz')
     else:
         print(i)
 
-#! Use List Comprehension to create a list of the first letters of every word in the string below:
+# ! Use List Comprehension to create a list of the first letters of every word in the string below:
 st = 'Create a list of the first letters of every word in this string'
-word_let=[word[0] for word in st.split()]
+word_let = [word[0] for word in st.split()]
 print(word_let)
+
+
+# ! МЕНШЕ З ДВОХ ПАРНИХ: Напишіть функцію, яка повертає менше з двох заданих чисел,
+# ! якщо обидва числа парні, але повертає більше, якщо одне чи обидва числа непарні.
+# lesser_of_two_evens(2,4) --> 2
+# lesser_of_two_evens(2,5) --> 5
+
+def lesser_of_two_evens(a, b):
+    if a % 2 == 0 and b % 2 == 0:
+        return min(a, b)
+    else:
+        return max(a, b)
+
+
+print(lesser_of_two_evens(4, 11))
+
+
+# ! ANIMAL CRACKERS: Напишіть, що функція приймає рядок із двох слів і повертає True,
+# ! якщо обидва слова починаються з однієї літери
+
+def animal_crackers(text):
+    wirdlist = text.split()
+    # функція перевіряє, чи є перша літера першого слова (знаходиться у wirdlist[0][0])
+    # рівною першій літері другого слова (знаходиться у wirdlist[1][0]).
+    return wirdlist[0][0] == wirdlist[1][0]
+
+
+print(animal_crackers('Levelheaded Rlama'))
+
+
+# ! задано два цілі числа, повертає True, якщо сума цілих чисел дорівнює 20 або
+# ! якщо одне з цілих чисел дорівнює 20. Якщо ні, повертає False
+
+def makes_twenty(n1, n2):
+    if (n1 + n2) == 20 or n1 == 20 or n2 == 20:
+        return True
+    else:
+        return False
+
+
+print(makes_twenty(20, 0))
+
+
+# ! СТАРИЙ МАКДОНАЛЬД: Напишіть функцію, яка робить першу та четверту літери імені великими
+
+def old_macdonald(name):
+    if len(name) > 3:
+        return name[:3].capitalize() + name[3:].capitalize()
+    # name[:3].capitalize() - цей вираз вибирає перші три символи рядка name
+    # за допомогою синтаксису зрізу (name[:3]), а потім застосовує до них метод .capitalize(),
+    # щоб перетворити їх на великі літери.
+    # name[3:].capitalize() - цей вираз вибирає всі символи рядка name починаючи
+    # з індексу 3 і до кінця рядка (name[3:]), а потім застосовує до них метод .capitalize(),
+    # щоб перетворити їх на великі літери.
+    else:
+        return 'Name is too short!'
+
+
+print(old_macdonald('macdonald'))
+
+
+# !MASTER YODA: Дано речення, поверніть речення зі словами, перевернутими
+# ! master_yoda('I am home') --> 'home am I'
+# ! master_yoda('We are ready') --> 'ready are We'
+
+def master_yoda(text):
+    return ' '.join(text.split()[::-1])
+
+
+print(master_yoda('home am I'))
+
+
+def almost_there(n):
+    return ((abs(100 - n) <= 10) or (abs(200 - n) <= 10))
+
+
+#print(almost_there(199))
+
+
+def almost_there1(n):
+    if 100 - n <= 10 or 200 - n <= 10:
+        return True
+    else:
+        return False
+
+
+#print(almost_there1(80))
+
+#!Маючи список int, поверніть True, якщо масив десь містить 3 поруч із 3.
+
+def find3(nums):
+    for n in range(0, len(nums)-1):
+        if nums[n] == 3 and nums[n+1] == 3:
+            return True
+
+    return False
+
+
+print(find3([1, 3, 2, 3]))
